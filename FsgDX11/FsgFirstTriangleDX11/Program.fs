375bfc1534629102262678cfe779cb6f528e5a65
Rahul <rahulgarg@live.ca>
2017-09-28 01:58:04 -0700

    First version of the FsgFirstTriangleDX11

---
+++ FsgDX11/FsgFirstTriangleDX11/Program.fs
@@ -0,0 +1,12 @@
+﻿(* 
+This file is part of fsharpgraphics project. 
+It is subject to the license terms in the LICENSE file found in the top-level directory of the project. 
+No part of fsharpgraphics project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.
+*)
+
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
