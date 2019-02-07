34a014b605ca1a8e4cbabd6a7186243d13ae676e
Rahul Garg <rahulgarg@live.ca>
2019-02-06 22:52:15 -0800

    Starting DX12 sample

---
+++ FsgDX12/FirstTriangle/Program.fs
@@ -0,0 +1,10 @@
+﻿(* Copyright 2019 Rahul Garg 
+This file is distributed as part of fsharpgraphics project under a BSD 3-clause license
+License terms can be found in LICENSE file distributed with the project
+*)
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
